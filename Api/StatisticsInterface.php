1266fa8c2045c2bc79e69417cbd5a52c6bc66252
Hugo Alliaume <kocal@live.fr>
2018-11-12 17:27:16 +0100

    feat: add parameter to provide a SMS number for missed calls

--- Api/StatisticsInterface.php
+++ Api/StatisticsInterface.php
@@ -3,7 +3,7 @@ namespace Yproximite\WannaSpeakBundle\Api;
 
 interface StatisticsInterface
 {
-    public function callTracking($method, $name, $phoneDest, $phoneDid, $platformId, $siteId, $callerId = false);
+    public function callTracking($method, $name, $phoneDest, $phoneDid, $platformId, $siteId, $callerId = false, $leg1 = null, $leg2 = null, $phoneMobileNumberForMissedCall = null);
 
     public function callTrackingDelete($didPhone);
 }
